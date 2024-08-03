378c41cdb9d90322ee77eabcf270e296439c4109
Marinas <puffins.tams_0d@icloud.com>
2024-08-03 20:08:10 +0300

    feat: brand filter

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { mapProductToCard } from "./utils/layout.js";
 
 document.addEventListener("DOMContentLoaded", displayAllProducts);
 const mainContainer = document.querySelector(".main");
+const brandFilterContainer = document.querySelector(".brand");
 
 async function displayAllProducts() {
   const products = await getAllProducts();
@@ -31,5 +32,46 @@ async function displayAllProducts() {
 
       localStorage.setItem("cart", JSON.stringify(cart));
     });
+  });
+  // Calculate the quantity of products for each brand
+  const brandCounts = products.reduce((acc, product) => {
+    acc[product.brand] = (acc[product.brand] || 0) + 1;
+    return acc;
+  }, {});
+
+  const uniqueBrands = Object.keys(brandCounts);
+
+  brandFilterContainer.innerHTML += uniqueBrands
+    .map(
+      (brand) =>
+        `
+    <div class="brand-filter flex">
+      <input type="checkbox" id="checkbox${brand}" name="brand" data-brand=${brand}>
+      <label for="checkbox${brand}" data-brand=${brand}>${brand}<span>(${brandCounts[brand]})</span></label>
+    </div>
+    `
+    )
+    .join("");
+
+  // Ensure only one checkbox can be checked at a time
+  brandFilterContainer.addEventListener("change", (e) => {
+    if (e.target.name === "brand") {
+      const checkboxes = document.querySelectorAll('input[name="brand"]');
+      checkboxes.forEach((checkbox) => {
+        if (checkbox !== e.target) {
+          checkbox.checked = false;
+        }
+      });
+    }
+  });
+
+  brandFilterContainer.addEventListener("click", (e) => {
+    const brand = e.target.getAttribute("data-brand");
+    if (brand) {
+      mainContainer.innerHTML = products
+        .filter((product) => brand === product.brand)
+        .map(mapProductToCard)
+        .join(" ");
+    }
   });
 }
